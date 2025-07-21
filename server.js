e0026c9e65aaaa959a43cb78ac18864157fe5c20
Hritik Pawar <73632558+unlikelycreator@users.noreply.github.com>
2025-07-21 15:04:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const port = 8084;
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: 'http://127.0.0.1:5501',
+  origin: ['http://127.0.0.1:5501', 'https://pastepalooza.netlify.app'],
   methods: ['GET', 'POST'],
   credentials: true
 }));
